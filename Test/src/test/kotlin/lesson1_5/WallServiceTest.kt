36a675e7344bb23e66a094a377878651c35ac386
Aleksandr Pavlov <konstantinovichpavlov@gmail.com>
2020-10-26 16:18:29 +0300

    выполнены задания в lesson1_5

--- Test/src/test/kotlin/lesson1_5/WallServiceTest.kt
+++ Test/src/test/kotlin/lesson1_5/WallServiceTest.kt
@@ -10,107 +10,133 @@ internal class WallServiceTest {
     @Test
     fun addPost_notZero() {
         val wallService = WallService()
-        var posts = mutableListOf<Post>()
         val firstView = Views(
             1
         )
-
         val firstRepost = Reposts(
             1,
             true
         )
-
         val firstComment = Comments(
-            1,
-            true,
-            true,
-            true,
-            true
+            count = 1,
+            canPost = true,
+            groupsCanPost = true,
+            canClose = true,
+            canOpen = true
         )
-
         val firstPost = Post(
-            0,
-            1,
-            1,
-            1,
-            1,
-            "Post",
-            1,
-            1,
-            true,
-            firstComment,
-            "PavlovAK",
-            firstRepost,
-            firstView,
-            1,
-            true,
-            true,
-            true,
-            true,
-            true,
-            true,
-            1
+            id = 0,
+            ownerId = 1,
+            fromId = 1,
+            createdBy = 1,
+            date = 1,
+            text = "Post",
+            replyOwnerId = 1,
+            replyPostId = 1,
+            friendsOnly = true,
+            comments = firstComment,
+            copyright = "PavlovAK",
+            reposts = firstRepost,
+            views = firstView,
+            signerId = 1,
+            canPin = true,
+            canDelete = true,
+            canEdit = true,
+            isPinned = true,
+            markedAsAds = true,
+            isFavorite = true,
+            postponedId = 1
         )
-
         val secondPost = firstPost.copy(id = 1)
-
-        val targetValue = secondPost.id
-        wallService.addPost(firstPost)
-        val result = posts.get(0).id
-
-        assertEquals(targetValue, result)
+        val result = wallService.addPost(firstPost)
+        assertEquals(secondPost, result)
     }
 
     @Test
     fun updatePost_removePostWithId() {
         val wallService = WallService()
-        val posts = mutableListOf<Post>()
-
         val firstView = Views(
             1
         )
-
         val firstRepost = Reposts(
             1,
             true
         )
-
         val firstComment = Comments(
-            1,
-            true,
-            true,
-            true,
-            true
+            count = 1,
+            canPost = true,
+            groupsCanPost = true,
+            canClose = true,
+            canOpen = true
         )
-
         val firstPost = Post(
-            0,
-            1,
-            1,
-            1,
-            1,
-            "Post",
-            1,
-            1,
-            true,
-            firstComment,
-            "PavlovAK",
-            firstRepost,
-            firstView,
-            1,
-            true,
-            true,
-            true,
-            true,
-            true,
-            true,
+            id = 0,
+            ownerId = 1,
+            fromId = 1,
+            createdBy = 1,
+            date = 1,
+            text = "Post",
+            replyOwnerId = 1,
+            replyPostId = 1,
+            friendsOnly = true,
+            comments = firstComment,
+            copyright = "PavlovAK",
+            reposts = firstRepost,
+            views = firstView,
+            signerId = 1,
+            canPin = true,
+            canDelete = true,
+            canEdit = true,
+            isPinned = true,
+            markedAsAds = true,
+            isFavorite = true,
+            postponedId = 1
+        )
+        val secondPost = wallService.addPost(firstPost)
+        assertTrue(wallService.updatePost(secondPost))
+    }
+
+    @Test
+    fun updatePost_removePostWithotId() {
+        val wallService = WallService()
+        val firstView = Views(
             1
         )
+        val firstRepost = Reposts(
+            1,
+            true
+        )
+        val firstComment = Comments(
+            count = 1,
+            canPost = true,
+            groupsCanPost = true,
+            canClose = true,
+            canOpen = true
+        )
+        val firstPost = Post(
+            id = 0,
+            ownerId = 1,
+            fromId = 1,
+            createdBy = 1,
+            date = 1,
+            text = "Post",
+            replyOwnerId = 1,
+            replyPostId = 1,
+            friendsOnly = true,
+            comments = firstComment,
+            copyright = "PavlovAK",
+            reposts = firstRepost,
+            views = firstView,
+            signerId = 1,
+            canPin = true,
+            canDelete = true,
+            canEdit = true,
+            isPinned = true,
+            markedAsAds = true,
+            isFavorite = true,
+            postponedId = 1
+        )
 
-        val secondPost = firstPost.copy (fromId = 999)
-        posts.add(firstPost)
-        wallService.addPost(secondPost)
-        assertTrue(wallService.updatePost(secondPost))
-
+        assertFalse(wallService.updatePost(firstPost))
     }
 }
